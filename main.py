7d3ed9c05f5a24cf76ba4e4dfa5b162cc13163ed
PauloBassani-s <paulo.roberto.bassani03@gmail.com>
2025-03-29 20:53:52 -0300

    Alteração de nomenclaturas
    Alterado a nomenclatura dos códigos

--- main.py
+++ main.py
@@ -4,4 +4,7 @@ app = FastAPI()
 
 @app.get("/")
 async def root ():
-    return {"message": "Hello World"}
+    return {"message": "atualização de código"}
+@app.get("/teste1")
+async def root ():
+    return {"message": "É nois queros"}
